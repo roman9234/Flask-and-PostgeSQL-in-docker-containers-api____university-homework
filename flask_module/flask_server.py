d62896f672f3e95275ddb7b35ecbd2e5104b7ff5
roman9234 <roman_lesovoy@mail.ru>
2024-07-18 22:26:11 +0300

    changed flask server fuctioning

--- flask_module/flask_server.py
+++ flask_module/flask_server.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 from flask_restful import Api, Resource
 import requests
+import json
+
+# TODO: сделать нормальное окружение
 
 app = Flask(__name__)
+
+
 # api = Api()
 
-class Vacancy():
+class Vacancy:
 
     def __init__(self, name, salary_min, salary_max, currency, city):
         self.name = name
@@ -14,14 +19,29 @@ class Vacancy():
         self.currency = currency
         self.city = city
 
+
 @app.route('/')
 def vacancies():
+    vacancies_list = []
+    res = requests.get("https://api.hh.ru/vacancies")
+    q = json.loads(res.text)
 
-    v1 = Vacancy("кино-оператор",20_000,60_000,"RUB", "Москва")
-    v2 = Vacancy("дизайнер сайтов",70_000,120_000,"RUB", "Санкт-Петербург")
-    v3 = Vacancy("Fullstack-программист",1_500,2_500,"USD", "Москва")
-    vacancies = [v1,v2,v3]
-    
+    # print(q['items'][0])
+
+    for x in q['items']:
+        try:
+            print(x['name'])
+            print(x['area']['name'])
+            print(x['salary']['from'])
+            print(x['salary']['to'])
+            print(x['salary']['currency'])
+            print(x['address']['raw'])
+
+            v = Vacancy(x['name'], x['salary']['from'], x['salary']['to'], x['salary']['currency'],x['address']['raw'])
+            vacancies_list.append(v)
+        except Exception as e:
+            print(e)
+
     # параметры сортировки
     sort_column = request.args.get('sort_column')
     sort_direction = request.args.get('sort_direction')
@@ -30,19 +50,21 @@ def vacancies():
     # Сортировка
     if sort_column:
         if sort_direction == "desc":
-            vacancies.sort(key=lambda x: getattr(x, sort_column), reverse=True)
+            vacancies_list.sort(key=lambda x: getattr(x, sort_column), reverse=True)
         else:
-            vacancies.sort(key=lambda x: getattr(x, sort_column))
+            vacancies_list.sort(key=lambda x: getattr(x, sort_column))
 
     # Фильтрация
     if filter_city:
-        vacancies = [vacancy for vacancy in vacancies if vacancy.city == filter_city]
+        vacancies_list = [vacancy for vacancy in vacancies_list if vacancy.city == filter_city]
 
-    return render_template("vacancies.html", vacancies=vacancies)
+    return render_template("vacancies.html", vacancies=vacancies_list)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
 
+# TODO удалить комментарии
 # python3 app.py
 # http://127.0.0.1:5000//main.py
 
